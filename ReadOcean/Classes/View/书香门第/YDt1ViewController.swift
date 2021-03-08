c7d3057c9aa91a727b59675540638ce3c3ddeab5
HAM02020 <1106201802@qq.com>
2021-03-08 17:18:16 +0800

    维护

--- ReadOcean/Classes/View/书香门第/YDt1ViewController.swift
+++ ReadOcean/Classes/View/书香门第/YDt1ViewController.swift
@@ -49,18 +49,20 @@ class YDt1ViewController : BaseViewController {
     }
     
     
-
+    
 
     private lazy var navView : HomeNavView = {
         let nav = HomeNavView()
         nav.searchBtnClickClosure = {
             print("搜索click")
-            let vc = DemoPickerVC()
+            let vc = YDSearchController()
+            vc.hidesBottomBarWhenPushed = true
             self.navigationController?.hidesBarsOnSwipe = false
+            
             self.navigationController?.pushViewController(vc, animated: true)
         }
         nav.categorysBtnClickClosure = {
-            print("搜索click")
+            print("分类click")
             let vc = BooksCategorysMainVC()
             vc.title = "分类书籍"
             self.navigationController?.hidesBarsOnSwipe = false
---
+++ ReadOcean/Classes/View/书香门第/搜索/YDSearchController.swift
@@ -0,0 +1,15 @@
+//
+//  YDSearchController.swift
+//  ReadOcean
+//
+//  Created by ruruzi on 2021/3/1.
+//  Copyright © 2021 HAM02020. All rights reserved.
+//
+
+import UIKit
+
+class YDSearchController:UISearchController{
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+}
